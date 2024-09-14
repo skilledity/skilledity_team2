f1961fc1961a135de2b1a9baad1f97763f03f1d9
amey0307 <ameytrips0307@gmail.com>
2024-09-14 12:58:27 +0530

    fix

--- index.js
+++ index.js
@@ -3,25 +3,11 @@ import school_router from "./routers/school.route.js";
 import student_router from './routers/student.route.js';
 import pool from "./model/school.model.js";
 import dotenv from 'dotenv';
-// import pkg from "pg";
-
-// const { Pool } = pkg;
-
-// const pool = new Pool({
-//     user: "postgres",
-//     host: "localhost",
-//     database: "skilledity",
-//     password: "admin",
-//     port: 5432
-// });
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
-
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
 app.use(express.json());
 
 //TESTING API
